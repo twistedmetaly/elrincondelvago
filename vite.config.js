ecb59de942c62262375445d043cf106c37b5bb1b
Nasco <twistedmetaly@gmail.com>
2023-12-30 01:22:58 -0500

    rollback renaming

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: 'https://elrincondelvago.github.io',
+    base: 'https://twistedmetaly.github.io/elrincondelvago',
     plugins: [react()],
 })
