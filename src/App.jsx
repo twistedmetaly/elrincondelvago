cb48b62bccd96516c63c4282524950ac76ccb8af
Nasco <twistedmetaly@gmail.com>
2024-01-06 18:31:31 -0500

    trying browser router in App with lazy and BasicLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {lazy} from "react";
+import Root from "./layout/Root.jsx";
+import BasicLayout from "./layout/BasicLayout.jsx";
+
 const HomeContainer = lazy(() => import('./pages/home/containers/HomeContainer.jsx'))
 
 function App() {
     return <BrowserRouter basename="/elrincondelvago">
         <Routes>
-            <Route path="/" element={<HomeContainer/>}/>
+            <Route path="/" element={<Root/>}>
+                <Route path="/" element={<BasicLayout/>}>
+                    <Route path="/" element={<HomeContainer/>}/>
+                </Route>
+            </Route>
+
         </Routes>
     </BrowserRouter>
 }
--- src/layout/BasicLayout.jsx
+++ src/layout/BasicLayout.jsx
@@ -3,6 +3,7 @@ import {Outlet} from "react-router-dom";
 const BasicLayout = () => {
     return <>
         {/* nav */}
+        <p>nav</p>
         {/* aside */}
         <Outlet/>
         {/* footer */}
