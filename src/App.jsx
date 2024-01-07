5f9a91daf080ecfa2673415d5b44ff55346e9ebc
Nasco <twistedmetaly@gmail.com>
2024-01-06 20:43:30 -0500

    trying router provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
 import './App.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {lazy} from "react";
-import Root from "./layout/Root.jsx";
-
-const BasicLayout = lazy(() => import('./layout/BasicLayout.jsx'))
-const HomeContainer = lazy(() => import('./pages/home/containers/HomeContainer.jsx'))
+import {RouterProvider} from "react-router-dom";
+import {router} from "./core/router/router.jsx";
 
 function App() {
-    return <BrowserRouter basename="/elrincondelvago">
-        <Routes>
-            <Route path="/" element={<Root/>}>
-                <Route path="/" element={<BasicLayout/>}>
-                    <Route path="/" element={<HomeContainer/>}/>
-                </Route>
-            </Route>
-
-        </Routes>
-    </BrowserRouter>
+    return <RouterProvider router={router}/>
 }
 
 export default App
